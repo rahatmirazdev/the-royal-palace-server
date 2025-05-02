5357cfd8c9367e9c5d482860dbf01d2ca30172dc
rahatmirazdev <rahatmiraz.dev@gmail.com>
2025-05-03 02:36:03 +0600

    Remove commented-out MongoDB connection confirmation code

--- index.js
+++ index.js
@@ -453,10 +453,6 @@ async function run() {
 
       res.json({ id: session.id });
     });
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   }
 }
